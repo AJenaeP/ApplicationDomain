18b23f717c8686b5492d77858c97be53b3464318
hlester7 <96212602+hlester7@users.noreply.github.com>
2023-03-20 18:58:03 -0400

    Added help page

--- compass/src/App.js
+++ compass/src/App.js
@@ -1,22 +1,22 @@
-import './App.css';
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/login_forgotPassword_newUser/Login"
-import ForgotPassword from './pages/login_forgotPassword_newUser/ForgotPassword';
-import React from 'react'
-import Header from './pages/Header';
-import NewUser from './pages/login_forgotPassword_newUser/NewUser';
-import { AuthProvider } from './utilities/AuthContext';
-import ProtectedRoute from './utilities/ProtectedRoute';
-import Home from './pages/Home';
-import PermissionDenied from './pages/PermissionDenied';
-import Sidebar from './utilities/Sidebar';
-import DashboardAdmin from './pages/DashboardAdmin';
+import Login from "./pages/login_forgotPassword_newUser/Login";
+import ForgotPassword from "./pages/login_forgotPassword_newUser/ForgotPassword";
+import React from "react";
+import Header from "./pages/Header";
+import NewUser from "./pages/login_forgotPassword_newUser/NewUser";
+import { AuthProvider } from "./utilities/AuthContext";
+import ProtectedRoute from "./utilities/ProtectedRoute";
+import Home from "./pages/Home";
+import PermissionDenied from "./pages/PermissionDenied";
+import Sidebar from "./utilities/Sidebar";
+import DashboardAdmin from "./pages/DashboardAdmin";
 /*import CreateNewUser from './pages/AdminPages/CreateNewUser';
 import ExpiredPasswords from './pages/AdminPages/ExpiredPasswords';*/
-import Manager from './pages/Manager';
-import Accountant from './pages/Accountant';
-import Accounts from './pages/AdminPages/Accounts';
-
+import Manager from "./pages/Manager";
+import Accountant from "./pages/Accountant";
+import Accounts from "./pages/AdminPages/Accounts";
+import HelpPage from "./pages/helppage";
 /* add to admin protected route after creation
     <Route path='/CreateNewUsers' element={<CreateNewUser />}></Route>
     <Route path='/Accounts' element={<Accounts />}></Route>
@@ -24,27 +24,40 @@ import Accounts from './pages/AdminPages/Accounts';
 */
 function App() {
   return (
-    <> 
-    <AuthProvider>
-      <Router>
+    <>
+      <AuthProvider>
+        <Router>
           <Routes>
-            <Route path='/' element={<Home/>}></Route>
-            <Route path='/home' element={<Home/>}></Route>
-            <Route path='/header' element={<Header/>}></Route>
-            <Route path='/login' element={<Login/>}></Route>
-            <Route path='/forgotpassword' element={<ForgotPassword/>}></Route>
-            <Route path='/newuser' element={<NewUser/>}></Route>
-            <Route path='/' element={<ProtectedRoute roleRequired='Administrator'/>}>
-              <Route path='/admindashboard' element={<DashboardAdmin/>}></Route>              
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/header" element={<Header />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
+            <Route path="/newuser" element={<NewUser />}></Route>
+            <Route
+              path="/"
+              element={<ProtectedRoute roleRequired="Administrator" />}
+            >
+              <Route
+                path="/admindashboard"
+                element={<DashboardAdmin />}
+              ></Route>
             </Route>
-            <Route path='/' element={<ProtectedRoute roleRequired='Manager'/>}>
-              
+            <Route path="/" element={<ProtectedRoute roleRequired="Manager" />}>
+              <Route path="/managerdashboard" element={<Manager />}></Route>
             </Route>
-            <Route path='/' element={<ProtectedRoute roleRequired='Accountant'/>}>
-              
+            <Route
+              path="/"
+              element={<ProtectedRoute roleRequired="Accountant" />}
+            >
+              <Route
+                path="/accountantdashboard"
+                element={<Accountant />}
+              ></Route>
             </Route>
-            <Route path='/denied' element={<PermissionDenied/>}></Route>
-            <Route path='/accounts' element={<Accounts/>}></Route>
+            <Route path="/denied" element={<PermissionDenied />}></Route>
+            <Route path="/accounts" element={<Accounts />}></Route>
+            <Route path="/helppage" element={<HelpPage />}></Route>
           </Routes>
         </Router>
       </AuthProvider>
---
+++ compass/src/pages/helppage.js
@@ -0,0 +1,52 @@
+//Logo import:
+import "../css/HelpPage.css";
+import React from "react";
+import Header from "./Header";
+
+//create code for image
+const HelpPage = () => {
+  return (
+    <>
+      <div className="HelpPage" id="outer-container">
+        <div>
+          <Header />
+        </div>
+        <head>
+          <link rel="stylesheet" href="css/HelpPage.css" />
+        </head>
+        <body>
+          <h1>Compass Credit Union Help</h1>
+          <br />
+          <h2>Accounts Page</h2>
+          <hr />
+          <h4>ADD ACCOUNT BUTTON:</h4>
+          <p>
+            After the add account button is pressed, a form will be generated
+            allowing a user to enter the account number, account name and other
+            relevant account information.
+          </p>
+
+          <h4>VIEW ACCOUNT BUTTON:</h4>
+          <p>
+            After the view account button is pressed the chart of accounts is
+            displayed.
+          </p>
+
+          <h4>EDIT ACCOUNT BUTTON:</h4>
+          <p>
+            After the edit account button is pressed the user will be able to
+            edit the selected account.
+          </p>
+
+          <h4>DELETE ACCOUNT BUTTON:</h4>
+          <p>
+            After the delete account button is pressed the selected account will
+            be deleted.
+          </p>
+        </body>
+      </div>
+    </>
+  );
+};
+
+export default HelpPage;
