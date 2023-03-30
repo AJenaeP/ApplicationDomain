3d37cd54767620486c0785ccc80d74d8a93ca36f
hlester7 <96212602+hlester7@users.noreply.github.com>
2023-03-29 20:45:22 -0400

    password attempt function updated

--- compass/src/pages/login_forgotPassword_newUser/Login.js
+++ compass/src/pages/login_forgotPassword_newUser/Login.js
@@ -1,204 +1,223 @@
-import React, { useEffect, useState } from 'react'
-import {  useNavigate, Link, useLocation } from 'react-router-dom'
-import compasslogo from '../../images/compassLogo.png';
-import { UserAuth } from '../../utilities/AuthContext'; 
+import React, { useEffect, useState } from "react";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import compasslogo from "../../images/compassLogo.png";
+import { UserAuth } from "../../utilities/AuthContext";
 import { FormControl, Container, TextField } from "@mui/material";
-import Button from '@mui/material/Button'
-import LoginIcon from '@mui/icons-material/Login';
-import '../../css/Login.css'
-import ClearIcon from '@mui/icons-material/Clear';
-import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+import Button from "@mui/material/Button";
+import LoginIcon from "@mui/icons-material/Login";
+import "../../css/Login.css";
+import ClearIcon from "@mui/icons-material/Clear";
+import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 
-import { green, red } from '@mui/material/colors';
+import { green, red } from "@mui/material/colors";
 
 export class user {
-    firstName;
-    lastName;
+  firstName;
+  lastName;
 }
 
 const Login = () => {
-    const navigate = useNavigate();
-    //const [error, setError] = useState('')
-    const [username, setUserName] = useState("")
-    const [password, setPassword] = useState("")
-    //values pulled from authcontext
-    const { signIn, user, isLoggedIn,userData, isDisabled } = UserAuth();
-    const [goToForgotPassword, setgoToForgotPassword] = useState(false);
-    const [goToHome, setgoToHome] = useState(false);
-    const [goToDashboard, setGoToDashboard] = useState(true);
+  const navigate = useNavigate();
+  //const [error, setError] = useState('')
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  //values pulled from authcontext
+  const { signIn, user, isLoggedIn, userData, isDisabled } = UserAuth();
+  const [goToForgotPassword, setgoToForgotPassword] = useState(false);
+  const [goToHome, setgoToHome] = useState(false);
+  const [goToDashboard, setGoToDashboard] = useState(true);
 
-
-    //this calls signin function from authcontext
-    const handleSignIn = (e) => {
-        e.preventDefault();
-        if (username === "") {
-            alert("Username can't be empty");
-        } else if (password === "") {
-            alert("Password can't be empty");
-        }
-        try{
-            signIn(username,password)
-        }catch(error){
-            console.log(error)
-        } finally {
-            handleNavigation()
-        }
+  //this calls signin function from authcontext
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    if (username === "") {
+      alert("Username can't be empty");
+    } else if (password === "") {
+      alert("Password can't be empty");
     }
-
-    function handleNavigation() {
-        if (user) {
-            if (goToDashboard) {
-                let role = window.localStorage.getItem('userRole')
-                if (role === "Administrator") {
-                    navigate('/admindashboard')
-                } else if (role === "Manager") {
-                    navigate('/managerdashboard')
-                } else if (role === "Accountant") {
-                    navigate('/accountantdashboard')
-                }
-            };
-        }
-        //navigate to forgot password page
-        if (goToForgotPassword) {
-            navigate('/forgotpassword')
-        };
-        //navigate to home screen
-        if (goToHome) {
-            navigate('/home')
-        };
+    try {
+      signIn(username, password);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      handleNavigation();
     }
+  };
 
-    return(
-        <div className="login">
-            <header>
-                <div className="title">
-                    <h1 className="company-name">Compass Credit Union</h1>
-                    <p className="slogan">
-                        Helping navigate the way for financial freedom yesterday, today, and
-                        tomorrow
-                    </p>
-                </div>
-                <div className="imgcontainer">
-                    <img src={compasslogo} alt="Avatar" className="picture" />
-                </div>
-            </header>
-            <Container className="form" style={{ width: 400, display: 'flex', alignItems: 'center', flexDirection: 'column'}}>
-                <div id='formHeader'>
-                    <h1>Login</h1>
-                    <p>Please fill in this form to login to your account.</p>
-                </div>
-                {!isDisabled &&  
-                    <FormControl id="formcontrol">                   
-                        <TextField
-                                id="outlined-password-input Username"
-                                label="Username"
-                                type="text"
-                                onChange={(e) => setUserName(e.target.value)}
-                                style={{marginBottom: 10, marginTop: 20}}
-                            />
-                            <TextField
-                                id="outlined-password-input password"
-                                label="Password"
-                                type="password"
-                                autoComplete="current-password"
-                                style={{ marginBottom: 60 }}
-                                onChange={e => setPassword(e.target.value)}
+  function handleNavigation() {
+    if (user) {
+      if (goToDashboard) {
+        let role = window.localStorage.getItem("userRole");
+        if (role === "Administrator") {
+          navigate("/admindashboard");
+        } else if (role === "Manager") {
+          navigate("/managerdashboard");
+        } else if (role === "Accountant") {
+          navigate("/accountantdashboard");
+        }
+      }
+    }
+    //navigate to forgot password page
+    if (goToForgotPassword) {
+      navigate("/forgotpassword");
+    }
+    //navigate to home screen
+    if (goToHome) {
+      navigate("/home");
+    }
+  }
 
-                            />
-                            <Button
-                                variant='outlined'
-                                size='large'
-                                type='submit'
-                                style={{ width: 225, marginBottom:10 }}
-                                startIcon={<LoginIcon />}
-                                className='submit'
-                                onClick={handleSignIn}
-                                sx={{ ':hover': { bgcolor: 'rgb(161, 252, 134,0.2)'}}}
-                            >
-                                Login
-                            </Button>
-                            <Button
-                                variant='outlined'
-                                size='large'
-                                style={{ width: 225, marginBottom:10 }}
-                                startIcon={<QuestionMarkIcon fontSize='14px' sx={{marginLeft: -2}}/>}
-                                className='forgot'
-                                onClick={() => { setgoToForgotPassword(true) }}
-                                sx={{ ':hover': { bgcolor: 'rgb(252, 229, 83,0.2)' } }}
-                            >
-                                Forgot Password
-                            </Button>
-                            <Button
-                                variant='outlined'
-                                size='large'
-                                style={{ width: 225, }}
-                                startIcon={<ClearIcon />}
-                                className='cancel'
-                                onClick={() => { setgoToHome(true) }}
-                                sx={{ ':hover': { bgcolor: 'rgb(252, 83, 83,0.2)' } }}
-                            >
-                                Cancel
-                            </Button>
-                    </FormControl>
-                }
-                {isDisabled &&
-                    <FormControl id="formcontrol">
-                        <TextField
-                            id="outlined-disabled Username"
-                            label="Disabled"
-                            type="text"
-                            onChange={(e) => setUserName(e.target.value)}
-                            style={{ marginBottom: 10, marginTop: 20 }}
-                        />
-                        <TextField
-                            id="outlined-disabled password"
-                            label="Disabled"
-                            type="password"
-                            autoComplete="current-password"
-                            style={{ marginBottom: 60 }}
-                            onChange={e => setPassword(e.target.value)}
-
-                        />
-                        <Button
-                            variant='outlined'
-                            size='large'
-                            type='submit'
-                            style={{ width: 225, marginBottom: 10 }}
-                            startIcon={<LoginIcon />}
-                            className='submit'
-                            sx={{ ':hover': { bgcolor: 'rgb(161, 252, 134,0.2)' } }}
-                            Disabled
-                        >
-                            Login
-                        </Button>
-                        <Button
-                            variant='outlined'
-                            size='large'
-                            style={{ width: 225, marginBottom: 10 }}
-                            startIcon={<QuestionMarkIcon fontSize='14px' sx={{ marginLeft: -2 }} />}
-                            className='forgot'
-                            onClick={() => { setgoToForgotPassword(true) }}
-                            sx={{ ':hover': { bgcolor: 'rgb(252, 229, 83,0.2)' } }}
-                        >
-                            Forgot Password
-                        </Button>
-                        <Button
-                            variant='outlined'
-                            size='large'
-                            style={{ width: 225, }}
-                            startIcon={<ClearIcon />}
-                            className='cancel'
-                            onClick={() => { setgoToHome(true) }}
-                            sx={{ ':hover': { bgcolor: 'rgb(252, 83, 83,0.2)' } }}
-                        >
-                            Cancel
-                        </Button>
-                    </FormControl>
-                }
-            </Container>
+  return (
+    <div className="login">
+      <header>
+        <div className="title">
+          <h1 className="company-name">Compass Credit Union</h1>
+          <p className="slogan">
+            Helping navigate the way for financial freedom yesterday, today, and
+            tomorrow
+          </p>
         </div>
-        
-        /*<div className="login">
+        <div className="imgcontainer">
+          <img src={compasslogo} alt="Avatar" className="picture" />
+        </div>
+      </header>
+      <Container
+        className="form"
+        style={{
+          width: 400,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div id="formHeader">
+          <h1>Login</h1>
+          <p>Please fill in this form to login to your account.</p>
+        </div>
+        {!isDisabled && (
+          <FormControl id="formcontrol">
+            <TextField
+              id="outlined-password-input Username"
+              label="Username"
+              type="text"
+              onChange={(e) => setUserName(e.target.value)}
+              style={{ marginBottom: 10, marginTop: 20 }}
+            />
+            <TextField
+              id="outlined-password-input password"
+              label="Password"
+              type="password"
+              autoComplete="current-password"
+              style={{ marginBottom: 60 }}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              variant="outlined"
+              size="large"
+              type="submit"
+              style={{ width: 225, marginBottom: 10 }}
+              startIcon={<LoginIcon />}
+              className="submit"
+              onClick={handleSignIn}
+              sx={{ ":hover": { bgcolor: "rgb(161, 252, 134,0.2)" } }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="outlined"
+              size="large"
+              style={{ width: 225, marginBottom: 10 }}
+              startIcon={
+                <QuestionMarkIcon fontSize="14px" sx={{ marginLeft: -2 }} />
+              }
+              className="forgot"
+              onClick={() => {
+                setgoToForgotPassword(true);
+              }}
+              sx={{ ":hover": { bgcolor: "rgb(252, 229, 83,0.2)" } }}
+            >
+              Forgot Password
+            </Button>
+            <Button
+              variant="outlined"
+              size="large"
+              style={{ width: 225 }}
+              startIcon={<ClearIcon />}
+              className="cancel"
+              onClick={() => {
+                setgoToHome(true);
+              }}
+              sx={{ ":hover": { bgcolor: "rgb(252, 83, 83,0.2)" } }}
+            >
+              Cancel
+            </Button>
+          </FormControl>
+        )}
+        {isDisabled && (
+          <FormControl id="formcontrol">
+            <TextField
+              disabled
+              id="outlined-disabled Username"
+              label="Disabled"
+              type="text"
+              onChange={(e) => setUserName(e.target.value)}
+              style={{ marginBottom: 10, marginTop: 20 }}
+            />
+            <TextField
+              disabled
+              id="outlined-disabled password"
+              label="Disabled"
+              type="password"
+              autoComplete="current-password"
+              style={{ marginBottom: 60 }}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              disabled
+              variant="outlined"
+              size="large"
+              type="submit"
+              style={{ width: 225, marginBottom: 10 }}
+              startIcon={<LoginIcon />}
+              className="submit"
+              sx={{ ":hover": { bgcolor: "rgb(161, 252, 134,0.2)" } }}
+            >
+              Login
+            </Button>
+            <Button
+              variant="outlined"
+              size="large"
+              style={{ width: 225, marginBottom: 10 }}
+              startIcon={
+                <QuestionMarkIcon fontSize="14px" sx={{ marginLeft: -2 }} />
+              }
+              className="forgot"
+              onClick={() => {
+                setgoToForgotPassword(true);
+              }}
+              sx={{ ":hover": { bgcolor: "rgb(252, 229, 83,0.2)" } }}
+            >
+              Forgot Password
+            </Button>
+            <Button
+              variant="outlined"
+              size="large"
+              style={{ width: 225 }}
+              startIcon={<ClearIcon />}
+              className="cancel"
+              onClick={() => {
+                setgoToHome(true);
+              }}
+              sx={{ ":hover": { bgcolor: "rgb(252, 83, 83,0.2)" } }}
+            >
+              Cancel
+            </Button>
+          </FormControl>
+        )}
+      </Container>
+    </div>
+
+    /*<div className="login">
         <header>
                 <p className="slogan">
                     Helping navigate the way for financial freedom yesterday, today, and
@@ -247,8 +266,8 @@ const Login = () => {
                 >New User?</button>
             </div>
         </div>*/
-    );
-}
+  );
+};
 
 //export { userData };
 //export { userObj };
--- compass/src/utilities/AuthContext.js
+++ compass/src/utilities/AuthContext.js
@@ -1,138 +1,148 @@
 import React, { useContext, useState, useEffect } from "react";
-import Header from '../pages/Header';
-import { auth, db } from './Firebase';
+import Header from "../pages/Header";
+import { auth, db } from "./Firebase";
 import { useNavigate, Navigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import {useAuthState}from 'react-firebase-hooks/auth'
+import { useAuthState } from "react-firebase-hooks/auth";
 import {
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    signOut,
-    onAuthStateChanged,
-    updatePassword,
-    getAuth,
-    reauthenticateWithCredential,
-    signInWithCredential,
-    EmailAuthProvider,
-    setPersistence,
-    browserLocalPersistence,
-}
-    from 'firebase/auth'
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+  updatePassword,
+  getAuth,
+  reauthenticateWithCredential,
+  signInWithCredential,
+  EmailAuthProvider,
+  setPersistence,
+  browserLocalPersistence,
+} from "firebase/auth";
 import {
-    getFirestore,
-    collection,
-    getDocs,
-    addDoc,
-    deleteDoc,
-    setDoc,
-    doc,
-    query,
-    where,
-}
-    from "firebase/firestore";
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  setDoc,
+  doc,
+  query,
+  where,
+} from "firebase/firestore";
 
 const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+  const [userData, setUserData] = useState({});
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isVerified, setIsVerified] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [loginAttempts, setLoginAttempts] = useState(2);
+  const decrementCounter = () => setLoginAttempts(loginAttempts - 1);
 
-    const [user, setUser] = useState({});
-    const [userData, setUserData] = useState({});
-    const [isLoggedIn, setIsLoggedIn] = useState(false)
-    const [isVerified, setIsVerified] = useState(false)
-    const [isDisabled, setIsDisabled] = useState(false)
-    const [loginAttempts, setLoginAttempts] = useState(3)
-    const decrementCounter = () => setLoginAttempts(loginAttempts - 1)
-
-    auth.onAuthStateChanged(user => {
-        if(user) {
-            const data = JSON.parse(window.localStorage.getItem('userData'));
-            setUserData(data)
-        } else {
-            console.log('user logged out')
-        }  
-    })
-    
-
-    const createUser = async (email, password, userInfo) => {
-        const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
-        setDoc(doc(db, "users", user.uid), userInfo);
-        //send email to admin account with userId, firstName, LastName and email
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      const data = JSON.parse(window.localStorage.getItem("userData"));
+      setUserData(data);
+    } else {
+      console.log("user logged out");
     }
+  });
 
-    const logout = () => {
-        signOut(auth).then(() => {
-            // Sign-out successful.
-            setIsLoggedIn(false)
-            localStorage.clear()
-        }).catch((error) => {
-            // An error happened.
-            alert(error)
-        });
-    }
+  const createUser = async (email, password, userInfo) => {
+    const userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    const user = userCredential.user;
+    setDoc(doc(db, "users", user.uid), userInfo);
+    //send email to admin account with userId, firstName, LastName and email
+  };
 
-    const signIn = async (username, password) => {
-        const userName = username
-        //console.log(userName)
-        const q = query(collection(db, "users"), where("userId", "==", userName));
+  const logout = () => {
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+        setIsLoggedIn(false);
+        localStorage.clear();
+      })
+      .catch((error) => {
+        // An error happened.
+        alert(error);
+      });
+  };
 
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            signInWithEmailAndPassword(auth, doc.data().email, password)
-                .then((userCredential) => {
-                    const user = userCredential.user;
-                    setUser(user);
-                    setUserData(doc.data());
-                    setIsLoggedIn(true)
-                    //console.log(user)
-                    //console.log(auth)
-                    console.log(doc.data())
-                    window.localStorage.setItem('userData', JSON.stringify(doc.data())) 
-                    window.localStorage.setItem('userRole', doc.data().role)
-                    //window.localStorage.setItem('user', JSON.stringify(user))
-                    //console.log(user)
-                }).catch((error) => {
-                    const errorCode = error.code;
-                    const errorMessage = error.message;
-                    alert(errorCode);
-                    if(errorCode === 'auth/wrong-password') {
-                       decrementCounter()
-                       if(loginAttempts == 0) {
-                        isDisabled(true)
-                       }
-                    }
-                });
-        });
-    }
+  const signIn = async (username, password) => {
+    const userName = username;
+    //console.log(userName)
+    const q = query(collection(db, "users"), where("userId", "==", userName));
 
-    const forgotPassword = async (email, username, secretQ1A, secretQ2A) => {
-        const q = query(collection(db, "users"), where("userId", "==", username));
-        const querySnapshot = await getDocs(q);
-        querySnapshot.forEach((doc) => {
-            if (doc.data().email !== email || doc.data().userId !== username) {
-                alert("No account was found with that email and password combination")
-            } else if (doc.data().secretQ1A !== secretQ1A || doc.data().secretQ2A !== secretQ2A) {
-                alert("one or both of your secret answer questions are invalid")
-            } else {
-                alert(" you've been verified, create a new password")
-                setIsVerified(true)
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      signInWithEmailAndPassword(auth, doc.data().email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          setUser(user);
+          setUserData(doc.data());
+          setIsLoggedIn(true);
+          //console.log(user)
+          //console.log(auth)
+          console.log(doc.data());
+          window.localStorage.setItem("userData", JSON.stringify(doc.data()));
+          window.localStorage.setItem("userRole", doc.data().role);
+          //window.localStorage.setItem('user', JSON.stringify(user))
+          //console.log(user)
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if (errorCode === "auth/wrong-password") {
+            decrementCounter();
+            alert(
+              "Incorrect Passcode: You have left " +
+                loginAttempts +
+                " login attempt;"
+            );
+            {
+              if (loginAttempts == 0) {
+                setIsDisabled(true);
+                console.log(isDisabled);
+              }
             }
+          }
         });
+    });
+  };
 
-    }
+  const forgotPassword = async (email, username, secretQ1A, secretQ2A) => {
+    const q = query(collection(db, "users"), where("userId", "==", username));
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      if (doc.data().email !== email || doc.data().userId !== username) {
+        alert("No account was found with that email and password combination");
+      } else if (
+        doc.data().secretQ1A !== secretQ1A ||
+        doc.data().secretQ2A !== secretQ2A
+      ) {
+        alert("one or both of your secret answer questions are invalid");
+      } else {
+        alert(" you've been verified, create a new password");
+        setIsVerified(true);
+      }
+    });
+  };
 
-
-
-    const newPassword = async (email, username, password) => {
-        console.log(auth.currentUser)
-        /*reauthenticate = (email) => {
+  const newPassword = async (email, username, password) => {
+    console.log(auth.currentUser);
+    /*reauthenticate = (email) => {
             var user = firebase.auth().currentUser;
             var cred = firebase.auth.EmailAuthProvider.credential(user.email, email);
             return user.reauthenticateWithCredential(cred);
 
         }*/
 
-            /*const user = auth.getUser;
+    /*const user = auth.getUser;
             updatePassword(user, password)
             .then(()=> {
                 console.log('update successful')
@@ -140,7 +150,7 @@ export const AuthProvider = ({ children }) => {
             .catch((error) => {
                 console.log(error)
             })*/
-        /*const userName = username
+    /*const userName = username
         //console.log(userName)
         const q = query(collection(db, "users"), where("userId", "==", userName));
         const querySnapshot = await getDocs(q);
@@ -152,50 +162,49 @@ export const AuthProvider = ({ children }) => {
             }).catch((error) => {
                 console.log(error)
             }))*/
-       // )*/
+    // )*/
+  };
 
+  const CurrentUser = () => {
+    const [user, loading, error] = useAuthState(auth);
+    if (loading) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
     }
-
-    const CurrentUser = (() => {
-        const [user, loading, error] = useAuthState(auth);
-        if (loading) {
-            return (
-                <div><p>Loading...</p></div>
-            )
-        }
-        if (error) {
-            return (
-                <div><p>Error: {error}</p></div>
-            )
-        }
-        if (user) {
-            console.log(user)
-            setUser(user)
-            console.log(user)
-        }
-    })
-
-    const value = {
-        createUser,
-        user,
-        userData,
-        isLoggedIn,
-        isVerified,
-        isDisabled,
-        logout,
-        signIn,
-        forgotPassword,
-        newPassword,
-        auth
+    if (error) {
+      return (
+        <div>
+          <p>Error: {error}</p>
+        </div>
+      );
     }
+    if (user) {
+      console.log(user);
+      setUser(user);
+      console.log(user);
+    }
+  };
 
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  const value = {
+    createUser,
+    user,
+    userData,
+    isLoggedIn,
+    isVerified,
+    isDisabled,
+    logout,
+    signIn,
+    forgotPassword,
+    newPassword,
+    auth,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
 
 export const UserAuth = () => {
-    return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
