44821dec907125f5dccdb35754466434f6e35c90
hrymer31 <123103874+hrymer31@users.noreply.github.com>
2023-04-02 16:25:06 -0400

    Update EditJournal.js
    Update to Role access

--- compass/src/pages/Journal/EditJournal.js
+++ compass/src/pages/Journal/EditJournal.js
@@ -14,6 +14,7 @@ import React, { useState, useEffect } from 'react'
 
 
 const EditJournal = ({ journal }) => {
+    const [role, setRole] = useState(window.localStorage.getItem('userRole'))
     const [updatedJournal, setUpdatedJournal] = useState({
        date: journal.selectedJournal.date,
        journalName: journal.selectedJournal.account_name,
@@ -28,7 +29,8 @@ const EditJournal = ({ journal }) => {
     function handleJournalAccountNameChange(e) { updatedJournal.account_name= e.target.value; }
     function handleJournalRefChange(e) { updatedJournal.ref= e.target.value; }
     function handleJournalDebitChange(e) { updatedJournal.debit = Number(e.target.value); } 
-    function handleJournalCreditChange(e) { updatedJournal.credit = Number(e.target.value); } 
+    function handleJournalCreditChange(e) { updatedJournal.credit = Number(e.target.value); }
+    function handleJournalStatusChange(e){updatedJournal.journal_status =e.target.value;} 
     
     function handleUpdate(e) {
         e.preventDefault();
@@ -53,10 +55,13 @@ const EditJournal = ({ journal }) => {
             },
         )
     }
-
+if (role !== "Accountant")
     
     return (
+        
+
         <>
+
             <DialogTitle> Edit An Entry</DialogTitle>
             <TextField
                 required
@@ -93,11 +98,82 @@ const EditJournal = ({ journal }) => {
                 onChange={handleJournalCreditChange}
             />
            
+            <InputLabel id="demo-simple-select-label status">Status</InputLabel>
+            <Select
+           
+                labelId="demo-simple-select-label status"
+                id="demo-simple-select"
+                label="Status"
+                sx={{ width: 'fit-content' }}
+                defaultValue={journal.selectedJournal.journal_status}
+                onChange={handleJournalStatusChange}
+            >
+                <MenuItem value='Approved'>Approved</MenuItem>
+                <MenuItem value='Pending'>Pending</MenuItem>
+                <MenuItem value='Rejected'>Rejected</MenuItem>
+                
+            </Select>
+
             <DialogActions>
                 <Button onClick={handleUpdate}>Update Entry</Button>
             </DialogActions> 
+
+           
         </>
+ 
     )
+    else
+        return (
+        
+
+            <>
+    
+                <DialogTitle> Edit An Entry</DialogTitle>
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Journal Date"
+                    defaultValue={journal.selectedJournal.date}
+                    onChange={handleJournalDateChange}
+                />
+                <TextField
+                    required
+                    id="outlined-required"
+                    label="Journal Name "
+                    defaultValue={journal.selectedJournal.account_name}
+                    onChange={handleJournalAccountNameChange}
+                />
+                 <TextField
+                    required
+                    id="outlined-required"
+                    label="Journal Ref"
+                    defaultValue={journal.selectedJournal.ref}
+                    onChange={handleJournalRefChange}
+                />
+        
+                <TextField
+                    id="outlined-required"
+                    label="Debit"
+                    defaultValue={journal.selectedJournal.debit}
+                    onChange={handleJournalDebitChange}
+                />
+                <TextField
+                    id="outlined-required"
+                    label="Credit"
+                    defaultValue={journal.selectedJournal.credit}
+                    onChange={handleJournalCreditChange}
+                />
+               
+    
+                <DialogActions>
+                    <Button onClick={handleUpdate}>Update Entry</Button>
+                </DialogActions> 
+    
+               
+            </>
+     
+        )
+    
 }
 
 
