96019b2d08259f2165f3f59e28b6e5ef95cbe156
hrymer31 <123103874+hrymer31@users.noreply.github.com>
2023-04-07 18:04:02 -0400

    Create ExpiredPasswords.js
    Expired Passwords update

---
+++ compass/src/pages/AdminPages/ExpiredPasswords.js
@@ -0,0 +1,78 @@
+import { 
+    TableBody, 
+    TableContainer, 
+    TableRow ,
+    TableCell,
+    Paper,
+    TableHead
+} from '@mui/material';
+import React, { useState, useEffect } from 'react'
+import { db }  from '../../utilities/Firebase';
+import { collection, query, where, getDocs, onSnapshot } from "firebase/firestore";
+import Header from '../Header';
+
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+    
+    const ref = collection(db, 'users')
+    const q = query(ref)
+
+    onSnapshot(q, (snapshot) => {
+        let usersList = []
+        snapshot.docs.forEach((doc) => {
+            usersList.push({ ...doc.data() })
+        })
+        setUsers(usersList)
+    })
+
+    return (
+        <div className='Users'>
+            <Header/>
+            <Paper
+                sx={{
+                    width: 1000,
+                    overflow: "hidden",
+                    display: "flex",
+                    position: "relative",
+                    top: 100,
+                    left: "10%",
+                }}
+            >
+                <TableContainer>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>UserId</TableCell>
+                            <TableCell>First Name</TableCell>
+                            <TableCell>Last Name</TableCell>
+                            <TableCell>Password Expiration Date</TableCell>
+                            <TableCell>Passwords</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            users.map((user, i) => {
+                                return (
+                                    <>
+                                        <TableRow
+                                            id={i}
+                                            key={i}
+                                        >
+                                            <TableCell> {user.userId}</TableCell>
+                                            <TableCell> {user.firstName}</TableCell>
+                                            <TableCell> {user.lastName}</TableCell>
+                                            <TableCell> {user.passwordExpiration}</TableCell>
+                                            <TableCell> {user.passwords}</TableCell>
+                                        </TableRow>
+                                    </>
+                                )
+                            })
+                        }
+                    </TableBody>
+                </TableContainer>
+            </Paper>
+        </div>
+    )
+}
+
+export default Users
