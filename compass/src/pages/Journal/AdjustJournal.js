3a9c1e132e9173db7f01856595fb4919465c2552
hrymer31 <123103874+hrymer31@users.noreply.github.com>
2023-04-16 20:56:08 -0400

    Manager Adjust Update

--- compass/src/pages/Journal/AdjustJournal.js
+++ compass/src/pages/Journal/AdjustJournal.js
@@ -22,6 +22,8 @@ const AdjustJournal = ({ journal }) => {
        debit: journal.selectedJournal.debit,
        credit: journal.selectedJournal.credit,
        journalexplanation: journal.selectedJournal.explanation,
+       journalStatus: journal.selectedJournal.journal_status,
+       comment: journal.selectedJournal.journal_comment
       
 //Need to add comment for reject 
     })
@@ -32,7 +34,9 @@ const AdjustJournal = ({ journal }) => {
     function handleJournalDebitAdjust(e) { adjustJournal.debit = Number(e.target.value); } 
     function handleJournalCreditAdjust(e) { adjustJournal.credit = Number(e.target.value); }
     function handleJournalExplanationAdjust(e) { adjustJournal.explanation = (e.target.value); }
-   
+    function handleAdjustStatusChange(e){adjustJournal.journal_status =e.target.value;} 
+    function handleAdjustCommentChange(e){adjustJournal.journal_comment =e.target.value;} 
+
     function handleAdjust(e) {
         e.preventDefault();
         console.log(journal.selectedJournal)
@@ -57,6 +61,7 @@ const AdjustJournal = ({ journal }) => {
         )
     }
 
+    if (role!== "Accountant ")
         return (
         
 
@@ -98,7 +103,87 @@ const AdjustJournal = ({ journal }) => {
                     onChange={handleJournalExplanationAdjust}
                 />
     
+
+    <InputLabel id="demo-simple-select-label status">Status</InputLabel>
+            <Select
+           
+                labelId="demo-simple-select-label status"
+                id="demo-simple-select"
+                label="Status"
+                sx={{ width: 'fit-content' }}
+                defaultValue={journal.selectedJournal.journal_status}
+                onChange={handleAdjustStatusChange}
+            >
+                <MenuItem value='Approved'>Approved</MenuItem>
+                <MenuItem value='Pending'>Pending</MenuItem>
+                <MenuItem value='Rejected'>Rejected</MenuItem>
+            </Select>
+
+            <InputLabel id="demo-simple-select-label status">Rejection Comment</InputLabel>
+            <Select
+           
+                labelId="demo-simple-select-label status"
+                id="demo-simple-select"
+                label="Comment"
+                sx={{ width: 'fit-content' }}
+                defaultValue={journal.selectedJournal.journal_comment}
+                onChange={handleAdjustCommentChange}
+            >
+                <MenuItem value='UnbalancedEntry'>Unbalanced Entry</MenuItem>
+                <MenuItem value='FailedPayment'>Failed Payment</MenuItem>
+                <MenuItem value='InaccurateAdjustment'>Inaccurate Adjustment</MenuItem>
+                <MenuItem value='Other'>Other - see email</MenuItem>
+            </Select>
                 <DialogActions>
+                    <Button onClick={handleAdjust}>Add Adjustment</Button>
+                </DialogActions> 
+    
+               
+            </>
+
+        )
+
+else
+return (
+    <>
+    
+    <DialogTitle> Adjust Entry</DialogTitle>
+
+   
+     <TextField
+        required
+        id="outlined-required"
+        label="Journal Ref"
+        style={{ color: 'red' }} 
+        defaultValue={journal.selectedJournal.ref}
+        onChange={handleJournalRefAdjust}
+    />
+
+    <TextField
+        id="outlined-required"
+        label="Debit"
+        style={{ color: 'red' }} 
+        defaultValue={journal.selectedJournal.debit}
+        onChange={handleJournalDebitAdjust}
+    />
+    <TextField
+        id="outlined-required"
+        label="Credit"
+        style={{ color: 'red' }} 
+        defaultValue={journal.selectedJournal.credit}
+        onChange={handleJournalCreditAdjust}
+    />
+   
+   <TextField
+        required
+        id="outlined-required"
+        label="Journal Explanation"
+        style={{ color: 'red' }} 
+        defaultValue={journal.selectedJournal.explanation}
+        onChange={handleJournalExplanationAdjust}
+    />
+
+<DialogActions>
                     <Button onClick={handleAdjust}>Add Adjustment</Button>
                 </DialogActions> 
     
